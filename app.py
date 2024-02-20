0979cdb2f3a1786daeedc6dc3623b01fa15a6dbd
zhillan Attarizal-R <zhilanar@gmail.com>
2024-02-20 10:09:58 +0700

    SPM

--- app.py
+++ app.py
@@ -1,22 +1,68 @@
 # Test Grounds
 import numpy as np
 
-X = np.array([[1,2,3],
-              [4,5,6],
-              [7,8,9],
+# def sigmoid(zt : float) -> float:
+#     return 1/np.exp(zt)
+
+# print(sigmoid(6))
+
+from bases import sigmoid
+
+
+X = np.array([[1, 2, 3],
+              [4, 5, 6],
+              [7, 8, 9],
               [10,11,12]])
 
-M = 3
+Y = np.array([[6],
+              [5],
+              [4],
+              [3]])
 
-# print(X.shape[1])
 
-rng = np.random.default_rng()
-a = 5 * rng.random((3, 2)) - 5
-# print(a)
+N = X.shape[0]
+M = X.shape[1]
+# print(N, M)
 
-from bases import random_arr
-# print(random_arr(4,3))
+# from bases import random_arr
+# w = random_arr(M, 1)
+# Z = random_arr(N, 1)
+# Yh = random_arr(N, 1)
+# e = random_arr(N, 1)
 
-from bases import perceptron
-p1 = perceptron(3)
-# print(p1.get_M(), p1.get_X(), p1.get_W())
+# w2 = w
+# Z2 = Z
+# Yh2 = Yh
+# e2 = e
+
+# for i in range(0, N):
+#     Z[i] = np.dot(X[i], w)
+#     Yh[i] = sigmoid(Z[i])
+#     e[i] = Y[i] - Yh[i]
+
+# print("New:\n\n", w, "\n\n", Z, "\n\n", Yh, "\n\n", e, "\n\n")
+
+# Z2 = np.dot(X, w2)
+# Yh2 = sigmoid(Z)
+# e2 = Y - Yh2
+
+# print("New 2 vectorized:\n\n", w, "\n\n", Z, "\n\n", Yh, "\n\n", e, "\n\n")
+
+# C = np.dot(e.transpose(), e)
+
+# print(C)
+
+
+
+X2 = np.array([[1, 2, 3],
+               [4, 5, 6],
+               [7, 8, 9],
+               [10,11,12]])
+
+# print(X * X2)
+
+from bases import SPM
+
+spm1 = SPM()
+spm1.train(X, Y, 0.1)
+spm1.show_results()
--- bases.py
+++ bases.py
@@ -1,56 +1,47 @@
+# 18220008 zhil
 # Perceptron Implementation in Python - Bases
-# Sumber: Matematika Untuk DL (M. Syamsuddin, 2021)
-
 import numpy as np
 
-# Y : Sig(XW)
-# X dim : (N,M)
-# Wt dim : (1, M)
-# Z = XW dim : (N, M)
-# N : 1..t..N Number of samples
-# M : Number of features
+# Math
+def sigmoid(zt : float) -> float:
+    return 1/np.exp(zt)
+def loss(y, yh):
+    return 0.5 * (y - yh)**2
 
-def sigmoid(Zt : np.ndarray):
-    # Fungsi Sigmoid
-    # Zt: Z ke-t, ndarray NxM
-    return 1/np.exp(-Zt)
-
-
+# Utils
 def random_arr(N : int, M : int) -> np.ndarray:
     # N, M: int
     rng = np.random.default_rng()
     return rng.random((N, M))
 
+class SPM:  # Single Perceptron Model
+    def __init__(self):
+        self.M = self.w = self.Z = self.Yh = self.e = self.C = None
+        # self.activation_function = sigmoid
+    
+    def forward_prop(self, X : np.ndarray, Y : np.ndarray):
+        N = X.shape[0]
+        self.M = X.shape[1]
+        self.w = random_arr(self.M, 1)
+        self.Z = random_arr(N, 1)
+        self.Yh = random_arr(N, 1)
+        self.e = random_arr(N, 1)
+        self.Z = np.dot(X, self.w)
+        self.Yh = sigmoid(self.Z)
+        self.e = Y - self.Yh
+        self.C = np.dot(self.e.transpose(), self.e)
 
-class perceptron:
-    # Minsky's Single Perceptron
-    # with Sigmoid Function
-    # only 1 output per t input
-    # atributes
-    def __init__(self, M : int):
-        self.M = M
-        self.X = None
-        self.W = random_arr(M, 1)
-        self.Y = None
-        # self.alpha = sigmoid function
-    # Setter
-    def set_M(self, M : int):
-        self.M = M
-        self.W = random_arr(M, 1)
-        print("Weights are reset")
-    def set_X(self, X : np.ndarray):
-        if X.shape[1] != self.M:
-            raise ValueError("Number of features did not match!")
-        self.X = X
-    def set_W(self, W : np.ndarray):
-        self.W = W
-    # Getter 
-    def get_M(self) -> int:
-        return self.M
-    def get_X(self) -> np.ndarray:
-        return self.X
-    def get_W(self) -> np.ndarray:
-        return self.W
-    # Z
-    def 
+    # Backwards propagation methods
+    def back_prop(self, X : np.ndarray, learning_rate : float):
+        w1 = self.w + learning_rate * np.dot(X.transpose(), (self.e * self.Yh * (1 - self.Yh)))
+        self.w = w1
+    
+    def train(self, X : np.ndarray, Y : np.ndarray, learning_rate : float) -> float:
+        self.forward_prop(X, Y)
+        self.back_prop(X, learning_rate)
+    
+    def show_results(self):
+        print(self.C)
+        print(self.Yh)
 
+# Sumber: Matematika Untuk DL (M. Syamsuddin, 2021)
